1f3789f2b6db0da517808fe0421fadd323e460bd
Aaron560 <aaron.ashcraft7@gmail.com>
2021-08-29 02:07:40 -0400

    BotCode

---
+++ Bot.py
@@ -0,0 +1,37 @@
+import discord
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
+
+client = discord.Client()
+RainbowSixQ = []
+#embed = discord.Embed(description = ("") color = discord.color.blue)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$join'):
+        await message.channel.send('Hello!'+ " " + str(message.author.display_name) + " " + "you have joined SpookyWagons's RainbowSixQ you are {}")
+        RainbowSixQ.append(str(message.author.display_name))
+        print(RainbowSixQ)
+    
+    if message.content.startswith('$list'):
+        await message.channel.send(RainbowSixQ)
+        
+    if message.content.startswith('$help'):
+        await message.channel.send("This is the help menu for this bot.")
+        
+        
+    if message.content.startswith('$delete'):
+        await message.channel.send("Removing Next player from Q. {0.RainbowSixQ}".format(RainbowSixQ[1:]))
+
+    
+        
+
+client.run(os.getenv('TOKEN'))
